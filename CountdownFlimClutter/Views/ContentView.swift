fb21140ebbf7e2e727fa2975b53bb29324e27768
Maciej Gomółka <mgomolka92@gmail.com>
2019-10-22 14:41:13 +0200

    Clean up project

--- CountdownFlimClutter/Views/ContentView.swift
+++ CountdownFlimClutter/Views/ContentView.swift
@@ -1,11 +1,3 @@
-//
-//  ContentView.swift
-//  CountdownFlimClutter
-//
-//  Created by Maciej Gomółka on 22/10/2019.
-//  Copyright © 2019 EL Passion. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
