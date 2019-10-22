9b62844430d2aa2936336deb78b01e657e3f1035
Maciej Gomółka <mgomolka92@gmail.com>
2019-10-22 14:35:16 +0200

    Initial Commit

---
+++ CountdownFlimClutter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CountdownFlimClutter
+//
+//  Created by Maciej Gomółka on 22/10/2019.
+//  Copyright © 2019 EL Passion. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
