e0b5e2d9fbfacd977d5a4c620e12d16236e72b46
Maciej Gomółka <mgomolka92@gmail.com>
2019-10-25 11:59:38 +0200

    Add gradient

--- CountdownFlimClutter/Views/FilmClutterContentView.swift
+++ CountdownFlimClutter/Views/FilmClutterContentView.swift
@@ -5,17 +5,34 @@ struct FilmClutterContentView: View {
     @EnvironmentObject var viewModel: FilmClutterViewModel
 
     var body: some View {
-        ZStack {
-            Color.filmBackground
-            FilmClutterProgressView()
-            SightShape()
-                .stroke(lineWidth: 3)
-                .fill(Color.sights)
-            Text(String(self.viewModel.countdownNumber))
-                .foregroundColor(Color.fontColor)
-                .fontWeight(.medium)
-                .font(.system(size: 160))
+        GeometryReader { geometry in
+            ZStack {
+                FilmClutterProgressView()
+                SightShape()
+                    .stroke(lineWidth: 3)
+                    .fill(Color.sights)
+                Text(String(self.viewModel.countdownNumber))
+                    .foregroundColor(Color.fontColor)
+                    .fontWeight(.medium)
+                    .font(.system(size: 160))
+            }.background(RadialGradient(
+                gradient: self.gradient,
+                center: .center,
+                startRadius: 0,
+                endRadius: max(geometry.size.height * 0.75, geometry.size.width * 0.75))
+            )
         }
+    }
+
+    private var gradient: Gradient {
+        Gradient(stops: colorStops)
+    }
+
+    private var colorStops: [Gradient.Stop] {
+        [
+            .init(color: .sights, location: 0),
+            .init(color: .black, location: 1)
+        ]
     }
 
 }
